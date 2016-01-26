53eec64b1317fd92374e59ede3357db454e94fef
Travis Lawrence <tlawrence3@ucmerced.edu>
2016-01-26 14:26:30 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
         long_description = f.read()
 
-setup(name = "python-bplogofun",
+setup(name = "bplogofun",
       install_requires=['statsmodels', 'numpy', 'scipy', 'pandas', 'patsy'],
       packages = ["bplogofun"],
       package_data={'bplogofun': ['eps/Template.eps']},
